a0cd701f2ea86e771ee4f40632bc06d8af721862
Abhinav Anand <abhinavanandzop@gmail.com>
2025-12-15 21:37:14 +0530

    Add files via upload

---
+++ Week 10/Question5/src/main/java/lang/oops/Main.java
@@ -0,0 +1,13 @@
+package lang.oops;
+public class Main {
+    public static void main(String[] args) {
+        CPlusPlus cpp = new CPlusPlus();
+
+        cpp.abstraction();
+        cpp.polymorphism();
+        cpp.inheritance();
+
+        cpp.templateMethod();
+        cpp.friendFunction();
+    }
+}
